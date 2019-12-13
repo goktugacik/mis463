8808db0ccb7175160070a589fc91977b8d2c36bd
goktugacik <goktugacik@gmail.com>
2019-12-13 17:15:13 +0300

    added results card

--- static/main.js
+++ static/main.js
@@ -1,7 +1,9 @@
+
+
 $(document).ready(function(){
     var counter = 1; //Initial field counter is 1
     var addButton = $('.add_button'); //Add button selector
-    var fieldHTML = '<div class="form-row"> <div class="col-4"> <input type="text" name="city[]" class="form-control" placeholder="City"> </div> <div class="col-2"> <input type="text" name="city[]" class="form-control" placeholder="Days"> </div> <div class="col-2"> <button type="button" class="btn btn-danger remove">X</button> </div> </div> '; //New input field html
+    var fieldHTML = '<div class="form-row"> <div class="col-4"> <input type="text" name="city[]" class="form-control" placeholder="City"> </div> <div class="col-3"> <input type="text" name="city[]" class="form-control" placeholder="Days"> </div> <div class="col-2"> <button type="button" class="btn btn-danger remove">X</button> </div> </div> '; //New input field html
     var calculateButtonHTML = '<div class="form-row"><input type="submit" id="calculateButton" class="btn btn-dark" value="Calculate"> </div>';
 
   //Once add button is clicked
@@ -31,6 +33,12 @@ $(document).ready(function(){
         data:$('#toAppend').serialize(),
         success:function(response){
             console.log(response);
+            var printable = JSON.stringify(response["Best"]);
+
+            $('#results').empty();
+
+            $('#results').append("***** <br>");
+            $('#results').append(printable);
         }
     });
 });
