f73923f0f26df45d1e5bdf6d3664a454eb875f4e
goktugacik <goktugacik@gmail.com>
2019-12-14 18:03:51 +0300

    Autocomplete fix

--- static/main.js
+++ static/main.js
@@ -4,17 +4,16 @@ $(document).ready(function(){
   BindControls();
   var counter = 1; //Initial field counter is 1
   var addButton = $('.add_button'); //Add button selector
-  var fieldHTML = '<div class="form-row"> <div class="col-5"> <input type="text" name="city[]" class="form-control" placeholder="City" required> </div> <div class="col-4"> <input type="number" name="city[]" class="form-control" placeholder="Days" required> </div> <div class="col-2"> <button type="button" class="btn btn-danger remove">X</button> </div> </div> '; //New input field html
+  var fieldHTML = '<div class="form-row"><div class="col-5"> <input type="text" name="city[]" class="form-control city" placeholder="City" required> </div> <div class="col-4"> <input type="number" name="city[]" class="form-control" placeholder="Days" min="1" required> </div> <div class="col-2"> <button type="button" class="btn btn-danger remove">X</button> </div> </div> '; //New input field html
   var calculateButtonHTML = '<div class="form-row"><input type="submit" id="calculateButton" class="btn btn-dark" value="Calculate"> </div>';
 
   //Once add button is clicked
   $("#addButton").click(function(){
-
     counter++; //Increment field counter
     $("#calculateButton").parent('div').remove();
     $('#toAppend').append(fieldHTML); //Add field html
     $('#toAppend').append(calculateButtonHTML); //Add calculateButton html
-
+    BindControls();
   });
 
 
@@ -23,6 +22,7 @@ $(document).ready(function(){
     e.preventDefault();
     $(this).parent('div').parent('div').remove(); //Remove field html
     x--; //Decrement field counter
+    BindControls();
   });
 
 
